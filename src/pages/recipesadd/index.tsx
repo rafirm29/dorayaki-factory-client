27cfba76743bcb91ec3dd20cc4dad1323d5fc7d8
Rafi Raihansyah <13519154@std.stei.itb.ac.id>
2021-11-23 10:44:28 +0700

    init pages and routes for recipes

---
+++ src/pages/recipesadd/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Template from '../../components/template';
+
+const recipes = () => {
+  return <Template title="Tambah Resep">a</Template>;
+};
+
+export default recipes;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,31 +1,43 @@
-import React from "react"
-import { Route } from "react-router-dom"
-import RouteGuard, { RouteProps } from "./routeGuard"
-import Login from "../pages/login"
-import Ingridients from "../pages/ingredients"
-import Request from "../pages/request"
-import Home from "../pages/home"
+import React from 'react';
+import { Route } from 'react-router-dom';
+import RouteGuard, { RouteProps } from './routeGuard';
+import Login from '../pages/login';
+import Ingridients from '../pages/ingredients';
+import Request from '../pages/request';
+import Home from '../pages/home';
+import RecipesList from '../pages/recipeslist';
+import RecipesAdd from '../pages/recipesadd';
 export const routes: RouteProps[] = [
   {
     Component: Home,
-    path: "/home",
+    path: '/home',
   },
   {
     Component: Login,
-    path: "/",
+    path: '/',
     beforeLoggedIn: true,
   },
   {
     Component: Ingridients,
-    path: "/ingredients",
+    path: '/ingredients',
     afterLoggedIn: true,
   },
   {
     Component: Request,
-    path: "/request",
+    path: '/request',
     afterLoggedIn: true,
   },
-]
+  {
+    Component: RecipesList,
+    path: '/recipes/list',
+    beforeLoggedIn: true,
+  },
+  {
+    Component: RecipesAdd,
+    path: '/recipes/add',
+    beforeLoggedIn: true,
+  },
+];
 
 const renderRoutes = () => {
   return routes.map((route) => (
@@ -38,7 +50,7 @@ const renderRoutes = () => {
         </RouteGuard>
       }
     ></Route>
-  ))
-}
+  ));
+};
 
-export default renderRoutes
+export default renderRoutes;
