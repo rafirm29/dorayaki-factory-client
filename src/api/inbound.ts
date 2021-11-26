799250b34b45abddbfe0ca4a2e80d561db067f37
Jason Stanley Yoman <jasonstanleyyoman@gmail.com>
2021-11-26 14:46:00 +0700

    acc inbound api

--- src/api/inbound.ts
+++ src/api/inbound.ts
@@ -1,16 +1,26 @@
 import { AxiosInstance } from "axios"
 import { Inbound } from "../interface/inbound/Inbound"
-import { BasePaginationResponse } from "../interface/Base"
+import { BasePaginationResponse, BaseResponse } from "../interface/Base"
 
 export const GetAllInbound = (
-  axios: AxiosInstance,
-  page: number
+    axios: AxiosInstance,
+    page: number
 ): Promise<BasePaginationResponse<Inbound>> => {
-  return axios
-    .get("/api/inbound", {
-      params: {
-        page,
-      },
-    })
-    .then((response) => response.data as BasePaginationResponse<Inbound>)
+    return axios
+        .get("/api/inbound", {
+            params: {
+                page,
+            },
+        })
+        .then((response) => response.data as BasePaginationResponse<Inbound>)
 }
+
+export const AcceptInbound = (
+    axios: AxiosInstance,
+    id: number
+): Promise<BasePaginationResponse<Inbound>> => {
+    return axios
+        .get("/api/inbound/accept/" + id)
+        .then((response) => response.data as BaseResponse)
+}
+  
